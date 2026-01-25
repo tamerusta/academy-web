f88493f9b0437a9bc0c4f944459f0feadcc004fc
Furkan Ünsalan <me@furkanunsalan.dev>
2026-01-25 17:01:51 +0300

    lint

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -24,78 +24,76 @@ export async function GET(req: NextRequest) {
     const imageUrl = searchParams.get("image") || "";
 
     return new ImageResponse(
-      (
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: primaryColor,
+          position: "relative",
+        }}
+      >
+        {/* Event Name - Top Left */}
         <div
           style={{
-            width: "100%",
-            height: "100%",
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            backgroundColor: primaryColor,
-            position: "relative",
+            position: "absolute",
+            top: 60,
+            left: 60,
+            fontSize: 60,
+            fontWeight: "bold",
+            color: "white",
+            maxWidth: "70%",
+            wordWrap: "break-word",
           }}
         >
-          {/* Event Name - Top Left */}
-          <div
-            style={{
-              position: "absolute",
-              top: 60,
-              left: 60,
-              fontSize: 60,
-              fontWeight: "bold",
-              color: "white",
-              maxWidth: "70%",
-              wordWrap: "break-word",
-            }}
-          >
-            {eventName}
-          </div>
+          {eventName}
+        </div>
 
-          {/* Location - Bottom Left */}
-          <div
+        {/* Location - Bottom Left */}
+        <div
+          style={{
+            position: "absolute",
+            bottom: 60,
+            left: 60,
+            fontSize: 32,
+            color: "white",
+          }}
+        >
+          {locationName}
+        </div>
+
+        <div
+          style={{
+            position: "absolute",
+            top: 60,
+            right: 60,
+            fontSize: 32,
+            color: "white",
+          }}
+        >
+          {eventTime}
+        </div>
+
+        {/* Image - Bottom Right */}
+        {imageUrl && (
+          <img
+            src={imageUrl}
             style={{
               position: "absolute",
               bottom: 60,
-              left: 60,
-              fontSize: 32,
-              color: "white",
-            }}
-          >
-            {locationName}
-          </div>
-
-          <div
-            style={{
-              position: "absolute",
-              top: 60,
               right: 60,
-              fontSize: 32,
-              color: "white",
+              width: 200,
+              height: 200,
+              objectFit: "cover",
+              borderRadius: 8,
             }}
-          >
-            {eventTime}
-          </div>
-
-          {/* Image - Bottom Right */}
-          {imageUrl && (
-            <img
-              src={imageUrl}
-              style={{
-                position: "absolute",
-                bottom: 60,
-                right: 60,
-                width: 200,
-                height: 200,
-                objectFit: "cover",
-                borderRadius: 8,
-              }}
-              alt="Event image"
-            />
-          )}
-        </div>
-      ),
+            alt="Event image"
+          />
+        )}
+      </div>,
       {
         width: 1200,
         height: 630,
